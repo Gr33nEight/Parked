b50b6d4f2c5aa1cf53efbf21f5ad472ebbce6c77
Natanael <Natanael.jop.app@gmail.com>
2022-09-20 15:18:52 +0200

    Initial Commit

---
+++ Parked/ParkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkedApp.swift
+//  Parked
+//
+//  Created by Natanael Jop on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ParkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
